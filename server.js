62758a16c237dc59a7e69552e465ac7093a12771
pushthebutton <36baker@gmail.com>
2015-04-07 17:53:21 +0200

    added socket

--- server.js
+++ server.js
@@ -1,30 +1,48 @@
-var http = require('http'),
-    fs = require('fs'),
-    // NEVER use a Sync function except at start-up!
-    index = fs.readFileSync(__dirname + '/index.html');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// Send index.html to all requests
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
-// Socket.io server listens to our app
-var io = require('socket.io').listen(app);
-
-// Send current time to all connected clients
-function sendTime() {
-    io.sockets.emit('time', { time: new Date().toJSON() });
-}
-
-// Send current time every 10 secs
-setInterval(sendTime, 10000);
-
-// Emit welcome message on connection
-io.sockets.on('connection', function(socket) {
-    socket.emit('welcome', { message: 'Welcome!' });
-
-    socket.on('i am client', console.log);
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
-app.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+
+//var http = require('http'),
+//    fs = require('fs'),
+//    // NEVER use a Sync function except at start-up!
+//    index = fs.readFileSync(__dirname + '/index.html');
+//
+//// Send index.html to all requests
+//var app = http.createServer(function(req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.end(index);
+//});
+//
+//// Socket.io server listens to our app
+//var io = require('socket.io').listen(app);
+//
+//// Send current time to all connected clients
+//function sendTime() {
+//    io.sockets.emit('time', { time: new Date().toJSON() });
+//}
+//
+//// Send current time every 10 secs
+//setInterval(sendTime, 10000);
+//
+//// Emit welcome message on connection
+//io.sockets.on('connection', function(socket) {
+//    socket.emit('welcome', { message: 'Welcome!' });
+//
+//    socket.on('i am client', console.log);
+//});
+//
+//app.listen(process.env.PORT || 3000);
