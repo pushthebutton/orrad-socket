db77a270c1eb82b74dbd90e7569db1a444eb67e0
pushthebutton <36baker@gmail.com>
2015-04-07 21:31:11 +0200

    working again

--- server.js
+++ server.js
@@ -1,14 +1,74 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var rooms = [];
+var roomBosses = [];
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile('index.html', { root: __dirname });
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+    socket.on('joinRoom', function (roomToJoin){
+        var roomIndex = rooms.indexOf(roomToJoin); 
+        if(roomIndex != -1){
+            socket.join(roomToJoin);
+            socket.boss = roomBosses[roomIndex];
+            console.log("joined " + roomBosses);
+            console.log(socket.roomBoss);
+        }else{
+           socket.emit("roomClosed"); 
+        }
+        socket.on('message', function (msg){
+            if(msg == 1){
+                console.log('got rad for ' + socket.boss);
+                //console.log(io.sockets.connected[socket.boss]);
+                //io.sockets.socket(socket.boss).emit('vote',1);
+            if (io.sockets.connected[socket.boss]) {
+                console.log("boss connected");
+                    io.sockets.connected[socket.boss].emit('vote', 1);
+                }
+            }else if(msg == 0){
+                console.log('got sad');
+                if (io.sockets.connected[socket.boss]) {
+                console.log("boss connected");
+                    io.sockets.connected[socket.boss].emit('vote', 0);
+                }
+            }
+        });
+    });
+            
+    socket.on('createRoom', function(createdRoom){
+        console.log("the boss of " +createdRoom + " is " + socket.id);
+        socket.orradRoom = createdRoom;
+        io.emit('roomCreated', createdRoom);
+        console.log("room created " + createdRoom);
+        console.log("room index " + rooms.indexOf(createdRoom));
+        if(rooms.indexOf(createdRoom) != -1){
+            socket.emit("roomExists");
+            return false;
+        }else{
+            socket.join(createdRoom);
+        }
+        rooms.push(createdRoom);
+        roomBosses.push(socket.id);
+        
+        return false;
+    });
+    socket.on('disconnect', function(){
+        var index = rooms.indexOf(socket.orradRoom);
+        if (index > -1) {
+            rooms.splice(index, 1);
+            roomBosses.splice(index, 1);
+        }
+        console.log(rooms);
+    });
 });
+io.on('roomCreated', function (fool) {
+    console.log(fool);
+});
+
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
